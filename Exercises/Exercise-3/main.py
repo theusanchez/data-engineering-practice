6ce0861e38cb6c6a3b706900aa5e6cc2dcd7b7cf
Matheus Sanchez <matheussanchez@wizsolucoes.com.br>
2023-01-26 18:19:19 -0300

    Exercicio 3 feito.

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,8 +1,30 @@
 import boto3
+import gzip
 
+client = boto3.client(
+    's3',
+    aws_access_key_id='XXXXXXXXXXXXXXXXXXXX',
+    aws_secret_access_key='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+)
 
 def main():
     # your code here
+    client.download_file('commoncrawl', 'crawl-data/CC-MAIN-2022-05/wet.paths.gz', 'file.gz')
+    
+    with gzip.open('file.gz', 'rb') as f:
+        file_content = f.read()
+
+        first_link = str((file_content.split(b"\n")[0]))
+        first_link = first_link[2:-1]
+        print(first_link)
+
+    client.download_file('commoncrawl', first_link, 'fileFromLink.gz')
+
+    with gzip.open('fileFromLink.gz', 'rb') as f2:
+        lines = f2.read()
+        for line in lines:
+            print(line)
+
     pass
 
 
