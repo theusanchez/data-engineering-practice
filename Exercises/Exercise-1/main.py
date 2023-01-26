0482533a2df6cd364f205dbeec464fd5b405daeb
Matheus Sanchez <matheussanchez@wizsolucoes.com.br>
2023-01-26 11:34:12 -0300

    Exercicios 1 e 2 feitos.

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,5 +1,10 @@
+# Importando bibliotecas que serão utilizadas.
+import zipfile
+import os
 import requests
 
+
+# Criando váriaveis que serão utilizadas.
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip',
@@ -9,10 +14,36 @@ download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip',
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
 ]
+file_names = []
+
+# Lidar com os diretórios da máquina.
+# print(os.getcwd()) # Comando para verificar qual é o diretório que está sendo utilizado no momento
+# print(os.listdir()) # Comando usado para listar diretórios
+
+os.mkdir('Downloaded_Files2')  # Comando usado para criar um novo diretório.
+os.chdir('Downloaded_Files2')  # Comando utilizado para mudar de diretório.
 
 
 def main():
-    # your code here
+
+    # Salvar os nomes dos arquivos através da URL.
+    for x in download_uris:
+        file_names.append(x.split('/')[-1])
+    
+    # Baixar os arquivos
+    for x in download_uris:
+        response = requests.get(x)
+        nomearquivo = x.split('/')[-1]
+        open(nomearquivo, 'wb').write(response.content)
+    
+    for x in file_names:
+        try:
+            with zipfile.ZipFile(x) as zip_ref:
+                zip_ref.extractall()
+        except Exception as e:
+            print(e)
+            pass
+        
     pass
 
 
--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,32 @@
+# Importando bibliotecas que serão utilizadas.
+import zipfile
+import os
 import requests
-import pandas
+import pandas as pd
+from bs4 import BeautifulSoup
 
+
+# Criando váriaveis que serão utilizadas.
+download_uri = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
 
 
 def main():
-    # your code here
-    pass
+
+    r = requests.get(download_uri)
+    soup = BeautifulSoup(r.text)
+    allTDs = soup.find_all('td')
+    
+    for td in allTDs:
+        if td.text == '2022-02-07 14:03  ':
+            filename = td.find_previous_sibling('td').text
+            break
+
+    pathFileDownload = download_uri + filename
+    dataframe = pd.read_csv(pathFileDownload)
+    dfOrdered = dataframe.sort_values(by=['HourlyDryBulbTemperature'], ascending=False)
+    print(dfOrdered)
+
+pass
 
 
 if __name__ == '__main__':
