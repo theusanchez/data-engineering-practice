91b16e92984f557a0eecd386c5586f69c0a13600
Matheus Sanchez <matheussanchez@wizsolucoes.com.br>
2023-01-30 21:58:24 -0300

    Exercicio 4 - Feito.

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,8 +1,25 @@
-import boto3
+import os
+import json
 
+rootdir = '/app/data'
 
 def main():
-    # your code here
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            extension = (file.split('.')[1])
+            if extension == 'json':
+                link = subdir.replace('\\', '/')
+                link = link + '/' + file
+                with open(link) as json_file:
+                    with open(file[0:-4] + 'csv', 'w') as fileToWrite:
+                        json_content = json_file.read()
+                        data = json.loads(json_content)
+                        data['geolocationType'] = data['geolocation']['type']
+                        data['geolocationCoordinates'] = data['geolocation']['coordinates']
+                        data.pop('geolocation')
+                        fileToWrite.write(str(data))
+                        fileToWrite.close()
+
     pass
 
 
